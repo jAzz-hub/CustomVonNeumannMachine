c203e2bfad34c519c69fa1c89cd03d493f524657
jAzz-hub <joaogu.2001@gmail.com>
2025-01-04 18:34:52 -0300

    <feat> :: Creating Core and Process as structs

--- src/main.cpp
+++ src/main.cpp
@@ -52,47 +52,16 @@ int main(int argc, char* argv[]){
 
     process p1 = process(argv[1], 3, "novo", 4828, 284);
     core c1 = core(ram, registers, UC, counterForEnd, counter, clock, endProgram, data, p1);
-
+    
+    c1.start();
+    
     pthread_t t1;
     pthread_create(&t1, NULL, tico, NULL);
 
     teco();
+
+    c1.running_asm();
+
     return 0;
 
-
-//   loadProgram(argv[1],ram);
-
-
-//             while(counterForEnd > 0){
-//                 if(counter >= 4 && counterForEnd >= 1){
-//                     //chamar a instrução de write back
-//                     UC.Write_Back(UC.data[counter - 4],ram,registers);
-//                 }
-//                 if(counter >= 3 && counterForEnd >= 2){
-//                     //chamar a instrução de memory_acess da unidade de controle
-//                     UC.Memory_Acess(registers, UC.data[counter - 3],ram);
-// }                }
-//                 if(counter >= 2 && counterForEnd >= 3){
-//                     //chamar a instrução de execução da unidade de controle
-//                     UC.Execute(registers,UC.data[counter - 2], counter, counterForEnd,endProgram,ram);
-//                 }
-//                 if(counter >= 1 && counterForEnd >= 4){
-//                     //chamar a instrução de decode da unidade de controle
-//                     UC.Decode(registers, UC.data[counter-1]);
-//                 }
-//                 if(counter >= 0 && counterForEnd == 5){
-//                     //chamar a instrução de fetch da unidade de controle
-//                     UC.data.push_back(data) ;
-//                     UC.Fetch(registers, endProgram,ram);
-//                 }
-//                 counter += 1;
-//                 clock += 1;
-//                 if(endProgram == true){
-//                     counterForEnd =- 1;
-//                 }
-//             }
-        
-      
-       
-//     return 0;
 }
