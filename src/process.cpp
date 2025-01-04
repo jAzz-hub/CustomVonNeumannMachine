a6228bf659e3cc6dfdc222e226b5107ce5641e0b
jAzz-hub <joaogu.2001@gmail.com>
2025-01-04 13:52:19 -0300

    adicionando process.cpp

---
+++ src/process.cpp
@@ -0,0 +1,10 @@
+#include "process.h"
+
+void process::print_attributes() const {
+    std::cout<<this->input_program<<std:: endl;
+    std::cout<<this->timestamp<<std:: endl;
+    std::cout<<this->state<<std:: endl;
+    std::cout<<this->quantum<<std:: endl;
+    std::cout<<this->id<<std:: endl;
+    std::cout<<this->mem_addr<<std:: endl;
+}
